53c1e8f55d3f4fc3068e43d53de41aa24c6967ac
FinleyConway <finleyconway03@gmail.com>
2026-05-10 13:24:01 +0100

    Renamed and added namespace to server

--- host/src/main.cpp
+++ host/src/main.cpp
@@ -6,7 +6,7 @@ void on_esp_restart(common::restart_esp_t) {
 }
 
 int main() {
-    tcp_server_t server;
+    host::tcp_server_t server;
     server.configure(ip::tcp::v4(), 8080);
     server.register_receive_callback<common::restart_esp_t, &on_esp_restart>();
     server.start();
